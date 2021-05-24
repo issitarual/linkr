c5e31b67057fe0a81111f48c127cd6b0bd90f0fe
coelhothay <thay.csb@gmail.com>
2021-05-24 20:00:56 -0300

    routes

---
+++ src/components/App.js
@@ -0,0 +1,36 @@
+import {BrouserRouter as Router, Switch, Route} from 'react-router-dom';
+
+export default function App () {
+
+    return (
+        <Router>
+            <Switch>
+                <Route to='/sign-up' exact>
+
+                </Route>
+                <Route to='/' exact>
+
+                </Route>
+                <Route to='/timeline' exact>
+                    
+                </Route>
+                <Route to='/my-posts' exact>
+                    
+                </Route>
+                <Route to='/hashtag/:hashtag' exact>
+                    
+                </Route>
+                <Route to='/user/:id' exact>
+                    
+                </Route>
+                <Route to='/my-likes' exact>
+                    
+                </Route>
+            </Switch>
+        </Router>
+
+
+    );
+
+}
+
