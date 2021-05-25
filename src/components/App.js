6f480025b065b7277ea5de76e3b7c650a7c0b39f
coelhothay <thay.csb@gmail.com>
2021-05-25 17:15:46 -0300

    global styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import GlobalStyle from './GlobalStyles';
 
 export default function App () {
 
     return (
         <Router>
+            <GlobalStyle />
             <Switch>
                 <Route to='/sign-up' exact>
 
