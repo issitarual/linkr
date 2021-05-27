e848d866bf4748f570af89f2d968052d1231ebc7
coelhothay <thay.csb@gmail.com>
2021-05-26 22:43:34 -0300

    correçao

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,5 @@
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import React from 'react';
-<<<<<<< HEAD
-import Home from './home/Home.js';
-import Register from './sign-up/SignUp.js';
-
-import GlobalStyle from './GlobalStyles';
-
-import UserContext from './UserContext';
-
-
-export default function App () {
-    const [user, setUser] = useState([]);
-
-    return (
-        <UserContext.Provider value={{user, setUser}}>
-            <Router>
-                <GlobalStyle />
-                <Switch>
-                    <Route to='/' exact>
-                        <Home />
-                    </Route>
-                    <Route to='/sign-up' exact>
-                        <Register />
-                    </Route>
-                    <Route to='/timeline' exact>
-                        
-                    </Route>
-                    <Route to='/my-posts' exact>
-                        
-                    </Route>
-                    <Route to='/hashtag/:hashtag' exact>
-                        
-                    </Route>
-                    <Route to='/user/:id' exact>
-                        
-                    </Route>
-                    <Route to='/my-likes' exact>
-                        
-                    </Route>
-                </Switch>
-            </Router>
-        </UserContext.Provider>
-
-=======
 import { useState } from "react";
 import UserContext from './UserContext';
 import Home from './/home/Home.js';
@@ -87,7 +44,6 @@ export default function App () {
             </Switch>
         </Router>
         </UserContext.Provider>
->>>>>>> origin/feature-novo-post
     );
 }
 
