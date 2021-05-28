377ef4dbc238f6c82594c23c625cdd508640cae6
Jean Carlo <jeancarlomo@hotmail.com>
2021-05-27 22:50:54 -0300

    adiçao de json.parse

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -69,9 +69,7 @@ export default function Timeline(){
        window.open(link)
     }
 
-    function changeLoad(){
-        setServerLoading(!serverLoading)   
-    }
+   
 
     function sendToHashtag(val){
        
