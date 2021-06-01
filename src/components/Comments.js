451c282bdfd5b9210468815e1c29e444195cf3d3
Laura Tissi <laura.tracierra@gmail.com>
2021-06-01 20:42:23 -0300

    estilizando o ion-icon

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,6 +1,46 @@
-export default function Comments(){
+import { ChatbubblesOutline } from 'react-ionicons';
+import styled from 'styled-components';
+import {useContext, useState} from 'react';
+import UserContext from './UserContext';
+import axios from 'axios';
+
+export default function Comments({post}){
+    const { user } = useContext(UserContext);
+    const [postComments, setPostComments] = useState([]);
+    function loadingComments(id){
+        const config = {
+            headers:{
+                'Authorization' : `Bearer ${user.token}`
+            }
+        }
+        const request = axios.get(`https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/posts/${id}/comments`,config);
+        request.then(success => setPostComments(success));
+        request.catch(error => alert("Ocorreu um erro, tente novamente"))
+    }
     return(
         <>
+            <Icone>
+                <ChatbubblesOutline 
+                    onClick={() => loadingComments(post.id)}
+                    color={'#fff'} 
+                    height="25px"
+                    width="25px"
+                    style={{ 
+                        position: 'absolute',
+                        bottom: '-40px',
+                        left: '-180px'
+                    }}
+                />
+            </Icone>
+            <h6>{post.commentCount} comments</h6>
         </>
     )
 }
+
+const Icone = styled.div`
+    margin-top: 10px;
+    padding: 0px!important;
+    width: 25px;
+    height: 25px;
+    position: relative;
+`;
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,13 +1,12 @@
-import NewPost from './Timeline/NewPost';
-import axios from 'axios';
 import ReactHashtag from "react-hashtag";
 import {useHistory} from 'react-router-dom';
-import ReactTooltip from 'react-tooltip';
 import Loader from "react-loader-spinner";
 import ActionsPost from './Timeline/ActionsPost';
-import { HeartOutline, HeartSharp, ChatbubblesOutline, RepeatOutline } from 'react-ionicons';
+import { RepeatOutline } from 'react-ionicons';
 import InputNewText from './Timeline/InputNewText';
 import Repost from './repost/Repost';
+import Likes from './likes';
+import Comments from './Comments'
 
 /*import de style components*/
 import {PostInfo,LinkDescription,Links,Hashtag,TimelinePosts,LinkDetails,UserName,NoPostsYet,PostContent} from '../components/timelineStyledComponents'
@@ -27,7 +26,6 @@ return(
             : (allPosts.length===0 
                 ? <NoPostsYet>{noPostsMessage}</NoPostsYet>
                 :allPosts.map((post)=>{
-                    console.log(post);
             return(
             <li key={post.id} id={post.id}>
                 {post["repostedBy"] ? 
@@ -36,67 +34,21 @@ return(
                         color={'#ffffff'}
                         height="20px"
                         width="20px"
+                        style={{margin: "0"}}
                     />
                     Re-posted by {user.user.username === post.repostedBy.username ? "you" : post.repostedBy.username}
                 </span>) : ""}
                 <div className = "oficialPost">
                     <div className='postLeft'>
                         <img src={post.user.avatar} onClick={()=>goToUserPosts(post.user.id)}/>
-                        <div className ="ion-icon" 
-                data-tip={
-                    olderLikes.map(n => n.id).includes(post.id) && !likedPosts.map(n => n.id).includes(post.id)?
-                    olderLikes.filter(n => n.id === post.id)[0].likes === 0? "0 pessoas":
-                    `${olderLikes.filter(n => n.id === post.id)[0].names[0]} ${olderLikes.filter(n => n.id === post.id)[0].likes - 1 > 0? `e outra(s) ${olderLikes.filter(n => n.id === post.id)[0].likes - 1} pessoas`: ""} `: 
-                    likedPosts.map(n => n.id).includes(post.id)? 
-                    likedPosts.filter(n => n.id === post.id)[0].likes === 1 ? "Somente você":
-                    likedPosts.filter(n => n.id === post.id)[0].likes === 2? `Você e ${likedPosts.filter(n => n.id === post.id)[0].names.filter(n => n !== user.user.username)}`:
-                    `Você, ${likedPosts.filter(n => n.id === post.id)[0].names.filter(n => n !== user.user.username)[0]} e outras ${likedPosts.filter(n => n.id === post.id)[0].likes - 2} pessoas`:                                        
-                    post.likes.length === 0? "0 pessoas":
-                    post.likes.length === 1? `${post.likes.map(n => n["user.username"]).filter(n => n !== user.user.username)[0]}`:
-                    post.likes.length === 2? `${post.likes.map(n => n["user.username"]).filter(n => n !== user.user.username)[0]} e  ${post.likes.map(n => n["user.username"]).filter(n => n !== user.user.username)[1]}`:
-                    `${post.likes.map(n => n["user.username"]).filter(n => n !== user.user.username)[0]},  ${post.likes.map(n => n["user.username"]).filter(n => n !== user.user.username)[1]} e outras ${post.likes.length -2} pessoas`
-                } 
-            >
-                {likedPosts.map(n=>n.id).includes(post.id)?                                  
-                <HeartSharp 
-                    onClick={() => like(post.id)}
-                    color={'#AC2B25'} 
-                    height="25px"
-                    width="25px"
-                />:
-                <HeartOutline 
-                    onClick={() => like(post.id)}
-                    color={'#fff'} 
-                    height="25px"
-                    width="25px"
+                <Likes 
+                    post={post} 
+                    olderLikes={olderLikes} 
+                    likedPosts = {likedPosts}
+                    user={user}
+                    like={like}
                 />
-                }
-                <ReactTooltip 
-                    type="light"
-                    textColor="#505050"
-                    place="bottom"
-                    effect="solid"
-                    border="5"
-                />
-            </div> 
-            <h6>
-                {
-                likedPosts.map(n => n.id).includes(post.id)?
-                likedPosts.filter(n => n.id === post.id)[0].likes:
-                olderLikes.map(n => n.id).includes(post.id)?
-                olderLikes.filter(n => n.id === post.id)[0].likes:
-                post.likes.length
-                } likes
-            </h6>
-            <div className = "ion-icon">
-                <ChatbubblesOutline 
-                    onClick={() => alert(post.id)}
-                    color={'#fff'} 
-                    height="25px"
-                    width="25px"
-                />
-            </div>
-            <h6>{post.commentCount} comments</h6>
+            <Comments post={post}/>
             <Repost id={post.id} />
             </div>
 
